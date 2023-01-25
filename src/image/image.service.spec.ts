0f5441b58fddb364bad7c7fe5c54322e50ef988e
Rodrigo Limões <rolimoes@gmail.com>
2023-01-25 09:27:29 -0300

    Ensure return the created image

--- src/image/image.service.spec.ts
+++ src/image/image.service.spec.ts
@@ -27,6 +27,29 @@ describe('ImageService', () => {
 		service = new ImageService(mockRepository);
 	});
 
+	describe('create', () => {
+		const input = {
+			name: 'test2.png',
+			description: 'test2 de descrição',
+			size: 11189,
+			key: 'e2745bc44f43d4c3f0ac157cf808f900-test2.png',
+		};
+
+		it('should return the created image', async () => {
+			const response = await service.create(input);
+
+			expect(response).toBeDefined();
+			expect(response).toMatchObject({
+				...input,
+				_id: '63cfde53c12118dcd67b654d',
+				url: `http://localhost:4700/img/e2745bc44f43d4c3f0ac157cf808f900-test2.png`,
+				createdAt: '2023-01-24T13:25:19.609Z',
+			});
+			expect(mockRepository.create).toBeCalledTimes(1);
+			expect(mockRepository.create).toBeCalledWith(input);
+		});
+	});
+
 	describe('findOne', () => {
 		let id = '63d089bdcd33c453c10568f4';
 

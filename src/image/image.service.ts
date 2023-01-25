55dfc3287f00ed2436f6cbb2cfbd8817902651b2
Rodrigo Limões <rolimoes@gmail.com>
2023-01-24 22:49:32 -0300

    Adding findOne in service

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,7 +1,10 @@
 import { Image, ImageDocument } from '@image/interfaces/image.interface';
+import { HttpException } from '@src/common/utils/HttpException';
+import { HttpStatusCode } from '@src/common/enums/HttpStatusCode';
 
 interface Repository {
 	create(image: Image): Promise<ImageDocument>;
+	findOne(id: string): Promise<ImageDocument | null>;
 }
 
 export class ImageService {
@@ -9,5 +12,18 @@ export class ImageService {
 
 	create = (image: Image): Promise<ImageDocument> => {
 		return this.repository.create(image);
+	};
+
+	findOne = async (id: string) => {
+		const image = await this.repository.findOne(id);
+
+		if (!image) {
+			throw new HttpException({
+				statusCode: HttpStatusCode.NOT_FOUND,
+				message: `Image ${id} not found`,
+			});
+		}
+
+		return image;
 	};
 }

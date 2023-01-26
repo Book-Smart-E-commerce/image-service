7e634ad83b7e4ed9a41e559235a21e99610a621c
Rodrigo Limões <rolimoes@gmail.com>
2023-01-25 23:01:47 -0300

    Adding cors in app middleware

--- src/middleware/app.ts
+++ src/middleware/app.ts
@@ -2,11 +2,13 @@ import express, { Express } from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import path from 'path';
+import cors from 'cors';
 
 const UPLOAD_PATH = path.join(__dirname, '..', '..', 'tmp', 'uploads');
 
 export const appMiddleware = (app: Express): void => {
 	app.use(bodyParser.json());
+	app.use(cors());
 	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use(morgan('dev'));
 	app.use('/img', express.static(UPLOAD_PATH));

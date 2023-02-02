ce3a1c3c351e0131f5520c48c026ff75f665bc34
Rodrigo Limões <rolimoes@gmail.com>
2023-02-02 01:19:54 -0300

    Setting up the babel;  Adding build script in package.json

---
+++ babel.config.js
@@ -0,0 +1,41 @@
+const getPath =
+	path =>
+	([, name]) => {
+		return `${path}${name}`;
+	};
+
+module.exports = {
+	presets: [
+		[
+			'@babel/preset-env',
+			{
+				targets: {
+					node: 18,
+				},
+			},
+		],
+		'@babel/preset-typescript',
+	],
+	plugins: [
+		[
+			require.resolve('babel-plugin-module-resolver'),
+			{
+				root: ['./src'],
+				alias: {
+					'@src': getPath('./src'),
+					'@middleware': getPath('./src/middleware'),
+					'@config': getPath('./src/config'),
+					'@image': getPath('./src/image'),
+				},
+			},
+		],
+		['@babel/plugin-proposal-decorators', { version: 'legacy' }],
+		[
+			'@babel/plugin-proposal-class-properties',
+			{
+				legacy: true,
+			},
+		],
+	],
+	ignore: ['**/*.spec.ts'],
+};

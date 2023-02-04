105a24f9f1c47128da3070c785be0c5736e5452a
Rodrigo Limões <rolimoes@gmail.com>
2023-02-04 18:49:20 -0300

    Adding Image Interface comment

--- src/image/interfaces/image.interface.ts
+++ src/image/interfaces/image.interface.ts
@@ -1,11 +1,29 @@
 import { HydratedDocument } from 'mongoose';
 
 export interface Image {
+	/**
+	 * Image Name
+	 */
 	name: string;
+	/**
+	 * Image Description
+	 */
 	description: string;
+	/**
+	 * File Size
+	 */
 	size: number;
+	/**
+	 * File key
+	 */
 	key: string;
+	/**
+	 * URL to access and view the image
+	 */
 	url: string;
+	/**
+	 * Image creation data
+	 */
 	createdAt?: string;
 }
 
